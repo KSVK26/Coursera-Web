ac15e2821c65a5d69d1e77b28a0bc51876a33f33
KSVK26 <ksvk26@gmail.com>
2023-03-26 13:31:27 +0530

    RESET

--- site/script.js
+++
@@ -1,7 +0,0 @@
-// Add an event listener for the navigation menu
-const navToggle = document.querySelector('.nav-toggle');
-const navMenu = document.querySelector('.nav-menu');
-
-navToggle.addEventListener('click', () => {
-  navMenu.classList.toggle('nav-menu--visible');
-});
